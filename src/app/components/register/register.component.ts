47e7fbf94d51dee8275e3d2e1fa123fef6b980d4
Ayush Kamble <aykkamble098@gmail.com>
2024-02-03 22:11:41 +0530

    commiting latest changes

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -3,34 +3,26 @@ import { User } from '../../Classes/User';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/config';
 
-@Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
-})
+@Component({ selector: 'app-register', templateUrl: './register.component.html', styleUrls: ['./register.component.css'] })
+
 export class RegisterComponent {
-  user: User = {
-    Username: '',
-    Password: '',
-    PhoneNumber: '',
-    Email: '',
-    City: '',
-  };
+  user: User = { Username: '', Password: '', PhoneNumber: '', Email: '', City: '' };
 
   apiurl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   onSubmitUser() {
-    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     var body = this.user;
     const requestBody = { body };
     this.http.post<any>(`${this.apiurl}/CreateUser`, requestBody, { headers }).subscribe((data) => {
-      {
-        alert ("Task has been stored successfully");
-      }},
-      (error) =>{
-        console.error(error);
-      }); 
+      { 
+        alert(data.requestBody); 
+        console.log(data.requestBody);
+      
+      }
+    },
+      (error) => { console.error(error); });
   }
 }
